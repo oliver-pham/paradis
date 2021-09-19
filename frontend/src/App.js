d809fb48beaf1f93066e05145483e5cf689e3bda
Phu Thong Pham <ptpham4@myseneca.ca>
2021-09-19 00:41:14 -0400

    Init boilerplate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,13 @@
-import { Switch, Route } from 'react-router-dom';
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import React, { Component } from 'react'
+import App from 'base-shell/lib'
+import MUIConfig from 'material-ui-shell/lib'
+import merge from 'base-shell/lib/utils/config'
+import _config from './config'
 
-import Home from './Home';
-import Explore from './Explore';
-import SignIn from './SignIn';
-import SignUp from './SignUp';
-import MenuAppBar from './MenuAppBar';
-import NotFound from './NotFound';
-import './App.css';
+const config = merge(MUIConfig, _config)
 
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: "#4f5b62",
-      main: "#263238",
-      dark: "#000a12",
-      contrastText: "#ffffff",
-    },
-    secondary: {
-      light: "#ffffff",
-      main: "#fafafa",
-      dark: "#c7c7c7",
-      contrastText: "#000000",
-    },
-  },
-});
-
-function App() {
-  return (
-    <div className='App'>
-      <ThemeProvider theme={theme}>
-        <header className='App-header'>
-          <MenuAppBar />
-        </header>
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route exact path='/explore'>
-            <Explore />
-          </Route>
-          <Route exact path='/sign-in'>
-            <SignIn />
-          </Route>
-          <Route exact path='/sign-up'>
-            <SignUp />
-          </Route>
-          <Route>
-            <NotFound />
-          </Route>
-        </Switch>
-      </ThemeProvider>
-    </div>
-  );
+export default class Paradis extends Component {
+  render() {
+    return <App config={config} />
+  }
 }
-
-export default App;
